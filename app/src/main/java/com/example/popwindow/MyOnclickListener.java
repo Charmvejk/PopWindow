84a556dbaeaa60b3b24ed80332d034a66ffb273c
weishuyong1996 <svipwsy@163.com>
2019-05-11 00:13:16 +0800

    Initial commit

---
+++ app/src/main/java/com/example/popwindow/MyOnclickListener.java
@@ -0,0 +1,11 @@
+package com.example.popwindow;
+
+import android.view.View;
+
+/**
+ * Created by MQ on 2017/3/13.
+ */
+
+public interface MyOnclickListener {
+     void onItemClick(View view, int position);
+}
